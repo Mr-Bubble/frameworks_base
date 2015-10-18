9f3a67684e62d502d07b332a9670b6bf41518dba
spaceman860 <spaceman8583@gmail.com>
2015-10-18 02:16:56 +0000

    Allow chromium to sign in
    Change-Id: I51bdeffb9d5ddff09b5b15199f4648bee166dc37

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -711,6 +711,13 @@ final class DefaultPermissionGrantPolicy {
                     && doesPackageSupportRuntimePermissions(ringtonePickerPackage)) {
                 grantRuntimePermissionsLPw(ringtonePickerPackage,
                         STORAGE_PERMISSIONS, true, userId);
+
+            // Chromium Sign-in
+            PackageParser.Package chromiumPackage = getDefaultProviderAuthorityPackageLPr(
+                    "org.chromium.chrome", userId);
+            if (chromiumPackage != null) {
+                grantRuntimePermissionsLPw(chromiumPackage, CONTACTS_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(chromiumPackage, STORAGE_PERMISSIONS, userId);
             }
 
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
